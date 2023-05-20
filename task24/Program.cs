fae3329c2ada08e8f6940d125e9d367656e26c8c
VALERIA <valeri.tereshenkova@icloud.com>
2023-05-20 10:59:46 +0300

    Task 24

---
+++ task24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int A)
+{
+    int sum = 0;
+
+    for(int i = 0; i <= A; i++)
+    {
+        sum+=i;
+    }
+
+    return sum;
+}
+
+int number = ReadInt("Введите число: ");
+int sum = SumNumbers(number);
+System.Console.WriteLine(sum);
