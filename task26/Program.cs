17f834c17fb1dc5e1f5ba9d85535e24fb1455e3c
VALERIA <valeri.tereshenkova@icloud.com>
2023-05-20 11:15:39 +0300

    Task 26

---
+++ task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadInt(string text)
+{
+  System.Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountNumbers(int number)
+{
+  int sum = 0;
+  while (number != 0)
+  {
+    number /= 10;
+    sum += 1;
+  }
+  return sum;
+}
+
+
+int number = ReadInt("Введите число ");
+
+System.Console.WriteLine(CountNumbers(number));
